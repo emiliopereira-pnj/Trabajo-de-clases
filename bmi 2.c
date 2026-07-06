daf295490e2e4589c8a47f4727d7c911f02984ac
emiliopereira-pnj <emiliopereirauni@gmail.com>
2026-07-05 21:22:31 -0300

    Add files via upload

---
+++ bmi 2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int opcion;
+    float peso;
+    float altura;
+    float re;
+    printf("quieres usar libras(1) o kilogramos(2)\n");
+    printf("opciones que elegiste (1/2):");
+    scanf("%d", &opcion);
+    if (opcion==1) {
+        printf("peso(libras):");
+        scanf("%f", &peso);
+        printf("altura(pulgadas):");
+        scanf("%f", &altura);
+        re=(peso*703)/(altura*altura);
+        printf("tu indice es de:%f \n",re);
+    } else {
+        printf("peso(kilos):");
+        scanf("%f", &peso);
+        printf("altura(metro):");
+        scanf("%f", &altura);
+        re= peso/(altura*altura);
+        printf("tu indice es de: %f \n",re);
+    }
+    printf("  indice   /condición\n");
+    printf("   <18     /bajo peso\n");
+    printf("18.5 a 24.9/normal\n");
+    printf(" 25 a 29.9 /sobrepeso\n");
+    printf("  >=30     /obesidad\n \n");
+
+    if (re < 18) {
+        printf ("ustedes tiene: bajo peso\n");
+    }
+    else if (re >= 18.5 && re <= 24.9) {
+        printf ("ustedes tiene: peso normal\n");
+    }
+    else if (re >= 25 && re <= 29.9) {
+        printf ("ustedes tiene: sobrepeso\n");
+    }
+    else if (re >= 30) {
+        printf ("ustedes tiene: obesidad\n");
+    }
+    return 0;
+}
